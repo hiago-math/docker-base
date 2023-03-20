f7bc4bbe00e3a35fb24dce87754dc372bc272312
Hiago Matheus <oliverhia.math@gmail.com>
2023-03-20 19:25:56 -0300

    Alterando .sh

--- docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
+++ docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
@@ -1,8 +1,8 @@
 echo 'Copiando configuração do ngnix do projeto para o container do mesmo...'
-docker cp $PWD/docker/build/nginx/gym-api.conf api-exemplo-container:/etc/nginx/conf.d/
+docker cp $PWD/docker/build/nginx/api-exemplo.conf api-exemplo-container:/etc/nginx/conf.d/
 
 echo 'Copiando configuração do ngnix para o nginx base...'
-docker cp $PWD/docker/build/nginx/gym-api.conf nginx:/etc/nginx/conf.d/
+docker cp $PWD/docker/build/nginx/api-exemplo.conf nginx:/etc/nginx/conf.d/
 
 echo "Teste de sinstaxe"
 docker exec nginx nginx -t
