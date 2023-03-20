fea413a9415ec4b0515daadd7d289c6b0204bc12
Hiago Matheus <oliverhia.math@gmail.com>
2023-03-20 17:45:13 -0300

    Fix: estrutura docker compartilhada e unica

--- docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
+++ docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
@@ -1,17 +1,17 @@
 echo 'Copiando configuração do ngnix para o o interno...'
-docker cp $PWD/docker/build/nginx/app-ponto.conf api-gerenciamento:/etc/nginx/conf.d/
+docker cp $PWD/docker/build/nginx/gym-api.conf api-exemplo-container:/etc/nginx/conf.d/
 
 echo 'Copiando configuração do ngnix para o base...'
-docker cp $PWD/docker/build/nginx/app-ponto.conf nginx:/etc/nginx/conf.d/
+docker cp $PWD/docker/build/nginx/gym-api.conf nginx:/etc/nginx/conf.d/
 
 echo "Teste de sinstaxe"
 docker exec nginx nginx -t
-docker exec api-gerenciamento nginx -t
+docker exec api-exemplo-container nginx -t
 
 
 
 echo 'Restartando NGINX do container ...'
-docker exec api-gerenciamento bash service nginx restart
+docker exec api-exemplo-container bash service nginx restart
 printf "\n"
 
 docker exec nginx bash service nginx restart
