f51055e1546d57cab3cb92eb1feafa2ff2ebf2fe
Hiago Matheus <oliverhia.math@gmail.com>
2023-03-20 17:46:16 -0300

    Fix: echo do .sh

--- docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
+++ docker-exemplo-projeto/estrutura-compartilhada/docker/init-docker-dev.sh
@@ -1,14 +1,12 @@
-echo 'Copiando configuração do ngnix para o o interno...'
+echo 'Copiando configuração do ngnix do projeto para o container do mesmo...'
 docker cp $PWD/docker/build/nginx/gym-api.conf api-exemplo-container:/etc/nginx/conf.d/
 
-echo 'Copiando configuração do ngnix para o base...'
+echo 'Copiando configuração do ngnix para o nginx base...'
 docker cp $PWD/docker/build/nginx/gym-api.conf nginx:/etc/nginx/conf.d/
 
 echo "Teste de sinstaxe"
 docker exec nginx nginx -t
 docker exec api-exemplo-container nginx -t
-
-
 
 echo 'Restartando NGINX do container ...'
 docker exec api-exemplo-container bash service nginx restart
