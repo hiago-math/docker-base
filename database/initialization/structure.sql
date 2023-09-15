49317c4913d261ead94abfa32549479ca9e3468a
Hiago Moreira <oliverhia.math@gmail.com>
2023-09-15 16:38:35 -0300

    fix: ajustando estrutura do mysql e nginx

---
+++ database/initialization/structure.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA `api-armazenamento`;
+CREATE SCHEMA `api-autorizacao`;
+CREATE SCHEMA `api-cadastro`;
+CREATE SCHEMA `api-cnab`;
+CREATE SCHEMA `api-criterios-elegibilidade`;
+CREATE SCHEMA `api-nosql`;
+CREATE SCHEMA `api-ocorrencia`;
+CREATE SCHEMA `api-processamento`;
+CREATE SCHEMA `api-relatorios`;
+CREATE SCHEMA `api_auditoria`;
+CREATE SCHEMA ott;
+CREATE SCHEMA `api-gerenciamento-manifesto`;
+
